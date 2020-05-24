c38dd9f04fbc86400142793529a6463cb51c29bc
Jordão Macedo <jordao.souza05@gmail.com>
2020-05-24 17:01:38 -0300

    Finish register and login pages

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSignInAlt } from 'react-icons/fa';
+
+import "./styles.scss";
+
+import logoImg from "./../../assets/logo.svg";
+import heroesImg from "./../../assets/heroes.png";
+
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero"/>
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID" />
+          <button type="submit" className="btn">Entrar</button>
+
+          <Link to="/register" className="link">
+            <FaSignInAlt size="16" color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt="Heroes"/>
+    </div>
+  );
+}
